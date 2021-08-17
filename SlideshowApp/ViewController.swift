60108d07dcc9ccdd3e493c7c7c20e2e7faaca932
nabeat <nabedubstep825@gmail.com>
2021-08-17 19:36:53 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by skull825nabe on 2021/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
